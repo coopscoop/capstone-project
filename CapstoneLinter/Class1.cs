9fa9a66e9deeffec9488c97f5e24a1990877c96e
Coop <coopscoop8515@gmail.com>
2025-09-24 17:05:00 -0400

    NOTE: was doing stuff in a different repo so i'm redoing it here bc different heads and i was running into problems fixing it. generally will be in line with what happened in the other repo

---
+++ CapstoneLinter/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CapstoneLinter;
+
+public class Class1
+{
+
+}
---
+++ CapstoneTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CapstoneTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
